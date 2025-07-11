d9f4478d0fccc28de90d86b43376d4547701af23
Jisma Varghese <jismavarghese34@gmail.com>
2025-07-11 11:42:19 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import AddProduct from './components/AddProduct';
+import SearchProduct from './components/SearchProduct';
+import DeleteProduct from './components/DeleteProduct';
+import ViewProduct from './components/ViewProduct';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <BrowserRouter>
+      
+      <Routes>
+
+        <Route path="/"element= { <AddProduct/>  }   />
+        <Route path="/Search" element= { <SearchProduct/>  }   />
+        <Route path="/Delete" element= { <DeleteProduct/>  }   />
+        <Route path="/View" element= { <ViewProduct/>  }   />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
